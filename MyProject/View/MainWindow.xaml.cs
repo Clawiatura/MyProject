8eb782ee96706bc789aa22167073f3aff0980bbf
Clawiatura <maksimov19992805@icloud.com>
2025-03-06 17:56:20 +0200

    Добавьте файлы проекта.

---
+++ MyProject/View/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MyProject.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+namespace YourNamespace // Замените на ваше пространство имен
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //MainFrame.Navigate(new MainWindow()); // Замените Page1 на вашу начальную страницу
+        }
+
+
+        private void OpenPersonalAccount_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new PageJoinUser()); // Создайте страницу PersonalAccountPage
+        }
+
+
+        private void Ideas_Click(object sender, RoutedEventArgs e)
+        {
+            // Логика для кнопки "Идеи для поездок" (например, переход на другую страницу)
+            MainFrame.Navigate(new TravelIDeasPage());
+        }
+
+        private void Tips_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new TutuTipsPage());
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new HelpPage());
+        }
+    }
+}
+
+
---
+++ MyProject/View/PageJoinUser.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YourNamespace;
+
+namespace MyProject.View
+{
+    /// <summary>
+    /// Логика взаимодействия для PageRegistr.xaml
+    /// </summary>
+    public partial class PageJoinUser : Page
+    {
+        public PageJoinUser()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // if (есть ли такой пользователь в базе)
+
+            NavigationService.Navigate(new PageClient());
+
+
+
+
+
+
+        }
+    }
+}
